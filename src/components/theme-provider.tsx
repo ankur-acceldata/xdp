1449da9ac92a10ba882bda3d571236e282d68e36
Ankur Acceldata <ankur@acceldata.io>
2025-02-21 10:32:07 +0530

    build fix

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,8 +1,31 @@
 'use client'
 
 import * as React from 'react'
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { ThemeProvider as NextThemesProvider, Attribute } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: { children: React.ReactNode }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ 
+  children, 
+  attribute, 
+  defaultTheme, 
+  enableSystem, 
+  disableTransitionOnChange, 
+  ...props 
+}: { 
+  children: React.ReactNode, 
+  attribute?: Attribute, 
+  defaultTheme?: string, 
+  enableSystem?: boolean, 
+  disableTransitionOnChange?: boolean 
+}) {
+  return (
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+      disableTransitionOnChange={disableTransitionOnChange}
+      {...props}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 } 
